63ab87cf05edbd4683dd018cf3023801fe3fe7b6
urbanpidgeon <urbanpidgeon@proton.me>
2025-09-03 17:16:04 +0800

    initial giin docs commit

---
+++ docs/astro.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeFlexoki from 'starlight-theme-flexoki'
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+      plugins: [starlightThemeFlexoki()],
+			title: 'My Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+      customCss: [
+        // Relative path to your custom CSS file
+        './src/styles/custom.css',
+      ],
+		}),
+	],
+  vite: {
+    server: {
+      watch: {
+        usePolling: true
+      }
+    }
+  }
+});
